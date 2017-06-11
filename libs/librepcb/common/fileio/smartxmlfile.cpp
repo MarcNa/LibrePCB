3af4d39f63c096af011fd849a3e8f8fc5b4644ee
U. Bruhin <urbibruhin@bluewin.ch>
2017-06-11 11:17:56 +0200

    Try using s-expressions file format

--- libs/librepcb/common/fileio/smartxmlfile.cpp
+++ libs/librepcb/common/fileio/smartxmlfile.cpp
@@ -56,7 +56,8 @@ std::unique_ptr<DomDocument> SmartXmlFile::parseFileAndBuildDomTree() const thro
 
 void SmartXmlFile::save(const DomDocument& domDocument, bool toOriginal) throw (Exception)
 {
-    const FilePath& filepath = prepareSaveAndReturnFilePath(toOriginal);
+    FilePath filepath = prepareSaveAndReturnFilePath(toOriginal);
+    filepath.setPath(filepath.toStr().replace(".xml", ".scm"));
     FileUtils::writeFile(filepath, domDocument.toByteArray());
     updateMembersAfterSaving(toOriginal);
 }
